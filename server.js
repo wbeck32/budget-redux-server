451bf262f4f3d453280498151cf5ce9489182508
Wendy Beck <webeck@gmail.com>
2017-09-06 09:57:24 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+/* eslint no-console: "off" */
+require('dotenv').config()
+const app = require('./src/app');
+const connect = require('./src/connect');
+const http = require('http');
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/budget-redux-dev';
+const port = process.env.PORT || 3001;
+const server = http.createServer(app);
+connect(dbUri);
+
+server.listen(port, () => {
+    console.log('server running on', server.address());
+});
