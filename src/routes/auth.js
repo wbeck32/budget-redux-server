74d39a15b27f3f8dc5f476b328c134aceaa6e493
Wendy Beck <webeck@gmail.com>
2017-10-31 16:36:47 -0700

    removing console.logs

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -13,7 +13,6 @@ router
   .post('/signup', hasEmailAndPassword, asyncIt(async (req, res, next) => {
     const { email, password } = req.body;
     const exists = await User.exists({ email });
-    // console.log(8, exists)
     if (exists) {
       throw {code: 400, name: 'Email in use'};
     }
@@ -21,24 +20,19 @@ router
     let user = new User(req.body);
 
     const hash = await user.generateHash(password);
-    // console.log(44, hash)
     user.password = hash;
     const withPassword = await user.save();
-    // console.log(50, withPassword)
 
     const tokenizedUser = await tokenService.sign(withPassword);
     tokenizedUser.user.password = null;
-    // console.log(55, tokenizedUser)
 
     return tokenizedUser;
   }))
   .post(
     '/signin',
     asyncIt(async (req, res, next) => {
-      // console.log(787878, req.body)
       const { email, password, token } = req.body;
       delete req.body.password;
-      // console.log(787878, req.body)
       const user = await User.findOne({ email }).select();
       if (!user || !user.comparePassword(password)) {
         throw { code: 401, name: 'User not found'};
--- src/routes/me.js
+++ src/routes/me.js
@@ -15,18 +15,15 @@ router
       const category = new Category(req.body);
       category.user = iD;
       const savedCat = await category.save();
-      console.log(111111, savedCat)
       const updatedUser = await User.findOneAndUpdate(
         { _id: iD },
         { $push: { categories: savedCat._id } },
         { new: true, runValidators: true }
       );
-      console.log(555, updatedUser)
       const userCats = await Category.aggregate([
         { $match: { user: updatedUser._id } },
         { $project: { name: 1, catAmount: 1, catRemaining: 1 } }
       ]);
-      console.log(1000, userCats)
       return userCats;
     })
   )
@@ -34,12 +31,10 @@ router
     '/category',
     asyncIt(async (req, res, next) => {
       const userId = req.user.user._id;
-      console.log(222, userId, req.user)
       const allCats = await Category.find([
         { $match: { user: userId } },
         { $project: { name: 1, catAmount: 1, catRemaining: 1, user: 1 } }
       ]);
-      console.log(888, allCats)
       return allCats;
     })
   )
